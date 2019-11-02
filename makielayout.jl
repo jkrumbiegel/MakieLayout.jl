dfd90b7966f9234bdff9b2187a386d6e2f7f1b15
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-11-02 13:11:51 +0100

    add lots of comments

--- makielayout.jl
+++ makielayout.jl
@@ -17,7 +17,7 @@ mutable struct LayoutedAxis
     scene::Scene
     xlabel::Node{String}
     ylabel::Node{String}
-    limits::Node{FRect2D}
+    limits::Node{FRect2D} # these should steer the camera, not used yet
 end
 
 width(b::BBox) = b.right - b.left
@@ -25,15 +25,28 @@ height(b::BBox) = b.top - b.bottom
 
 abstract type Alignable end
 
+"""
+Used to specify space that is occupied in a grid. Like 1:1|1:1 for the first square,
+or 2:3|1:4 for a rect over the 2nd and 3rd row and the first four columns.
+"""
 struct Span
     rows::UnitRange{Int64}
     cols::UnitRange{Int64}
 end
 
+"""
+An object that can be aligned that also specifies how much space it occupies in
+a grid via its span.
+"""
 struct SpannedAlignable{T<:Alignable}
     al::T
     sp::Span
 end
+
+"""
+These functions tell whether an object in a grid touches the left, top, etc. border
+of the grid. This means that it is relevant for the grid's own protrusion on that side.
+"""
 
 isleftmostin(sp::SpannedAlignable, grid) = sp.sp.cols.start == 1
 isrightmostin(sp::SpannedAlignable, grid) = sp.sp.cols.stop == grid.ncols
@@ -72,15 +85,24 @@ struct GridLayout <: Alignable
     rowgapfraction::Float64
 end
 
+"""
+All the protrusion functions calculate how much stuff "sticks out" of a layoutable object.
+This is so that collisions are avoided, while what is actually aligned is the
+"important" edges of the layout objects.
+"""
+
 leftprotrusion(u::AxisLayout) = u.decorations.left
 leftprotrusion(s::SolvedAxisLayout) = s.inner.left - s.outer.left
 leftprotrusion(sp::SpannedAlignable) = leftprotrusion(sp.al)
 
 function leftprotrusion(gl::GridLayout)
+    # any objects that stick out on this side?
     leftmosts = filter(x -> isleftmostin(x, gl), gl.content)
     if isempty(leftmosts)
+        # no protrusion
         0.0
     else
+        # use the biggest protrusion of all objects that stick out
         maximum(leftprotrusion.(leftmosts))
     end
 end
@@ -124,76 +146,122 @@ function bottomprotrusion(gl::GridLayout)
     end
 end
 
-function solve(gl::GridLayout, bbox::BBox) # when the grid is inside some other grid
+"""
+This function solves a grid layout such that the "important lines" fit exactly
+into a given bounding box. This means that the protrusions of all objects inside
+the grid are not taken into account. This is needed if the grid is itself placed
+inside another grid.
+"""
+function solve(gl::GridLayout, bbox::BBox)
+
+    # first determine how big the protrusions on each side of all columns and rows are
     maxcollefts = zeros(gl.ncols)
     maxcolrights = zeros(gl.ncols)
     maxrowtops = zeros(gl.nrows)
     maxrowbottoms = zeros(gl.nrows)
 
+    # go through all the layout objects placed in the grid
     for c in gl.content
+        # determine the rows and columns where they start and end
         ileft = c.sp.cols.start
         iright = c.sp.cols.stop
         itop = c.sp.rows.start
         ibottom = c.sp.rows.stop
 
+        # increase the protrusions of those columns and rows if they are larger than
+        # what they were before.
+        # this way the gap between two columns is at least the largest protrusion
+        # sticking in from the left plus the largest sticking in from the right
         maxcollefts[ileft] = max(maxcollefts[ileft], leftprotrusion(c.al))
         maxcolrights[iright] = max(maxcolrights[iright], rightprotrusion(c.al))
         maxrowtops[itop] = max(maxrowtops[itop], topprotrusion(c.al))
         maxrowbottoms[ibottom] = max(maxrowbottoms[ibottom], bottomprotrusion(c.al))
     end
 
+    # compute what size the gaps between rows and columns need to be
     colgaps = maxcollefts[2:end] .+ maxcolrights[1:end-1]
     rowgaps = maxrowtops[2:end] .+ maxrowbottoms[1:end-1]
 
+    # determine the biggest gap
+    # using the biggest gap size for all gaps will make the layout more even, but one
+    # could make this aspect customizable, because it might waste space
     maxcolgap = maximum(colgaps)
     maxrowgap = maximum(rowgaps)
 
+    # determine the vertical and horizontal space needed just for the gaps
+    # again, the gaps are what the protrusions stick into, so they are not actually "empty"
+    # depending on what sticks out of the plots
     sumcolgaps = maxcolgap * (gl.ncols - 1)
     sumrowgaps = maxrowgap * (gl.nrows - 1)
 
-    # removed prots
+    # compute what space remains for the inner parts of the plots
     remaininghorizontalspace = width(bbox) - sumcolgaps
     remainingverticalspace = height(bbox) - sumrowgaps
 
+    # compute how much gap to add, in case e.g. labels are too close together
+    # this is given as a fraction of the space used for the inner parts of the plots
+    # so far, but maybe this should just be an absolute pixel value so it doesn't change
+    # when resizing the window
     addedcolgap = gl.colgapfraction * remaininghorizontalspace
     addedrowgap = gl.rowgapfraction * remainingverticalspace
 
+    # compute the actual space available for the rows and columns (plots without protrusions)
     spaceforcolumns = remaininghorizontalspace - addedcolgap * (gl.ncols - 1)
     spaceforrows = remainingverticalspace - addedrowgap * (gl.nrows - 1)
 
+    # compute the column widths and row heights using the specified row and column ratios
     colwidths = gl.colratios ./ sum(gl.colratios) .* spaceforcolumns
     rowheights = gl.rowratios ./ sum(gl.rowratios) .* spaceforrows
 
+    # this is the vertical / horizontal space between the inner lines of all plots
     colgap = maxcolgap + addedcolgap
     rowgap = maxrowgap + addedrowgap
 
-    # removed leftprot
+    # compute the x values for all left and right column boundaries
     xleftcols = [bbox.left + sum(colwidths[1:i-1]) + (i - 1) * colgap for i in 1:gl.ncols]
     xrightcols = xleftcols .+ colwidths
 
-    # removed topprot
+    # compute the y values for all top and bottom row boundaries
     ytoprows = [bbox.top - sum(rowheights[1:i-1]) - (i - 1) * rowgap for i in 1:gl.nrows]
     ybottomrows = ytoprows .- rowheights
 
+    # now we can solve the content thats inside the grid because we know where each
+    # column and row is placed, how wide it is, etc.
+    # note that what we did at the top was determine the protrusions of all grid content,
+    # but we know the protrusions before we know how much space each plot actually has
+    # because the protrusions should be static (like tick labels etc don't change size with the plot)
     solvedcontent = SpannedAlignable[]
     for c in gl.content
+        # determine in which rows and columns the content object lies
         ileft = c.sp.cols.start
         iright = c.sp.cols.stop
         itop = c.sp.rows.start
         ibottom = c.sp.rows.stop
 
+        # make a bounding box with the x and y values of the columns and rows
         bbox_cell = Main.BBox(
             xleftcols[ileft], xrightcols[iright], ytoprows[itop], ybottomrows[ibottom])
 
+        # solve the child object's layout
+        # this is what makes nested grids possible, because they just get solved from the top
+        # all the way down
         solved = solve(c.al, bbox_cell)
+
+        # add the solved object to the list of solved child objects that gets saved in the solved layout
         push!(solvedcontent, SpannedAlignable(solved, c.sp))
     end
 
-    # solvedcontent = solve.(gl.content)
+    # return a solved grid layout in which all objects are also solved layout objects
     SolvedGridLayout(bbox, solvedcontent, gl.nrows, gl.ncols, xleftcols,
         xrightcols, ytoprows, ybottomrows)
 end
 
+"""
+This function solves a grid layout so that it fits exactly inside a bounding box.
+Exactly means that the protrusions of all other objects inside this grid layout
+also have to fit into the bounding box. This is needed if the grid is the outermost
+object in the layout, the bounding box would then be the scene boundary.
+"""
 function outersolve(gl::GridLayout, bbox::BBox)
     maxcollefts = zeros(gl.ncols)
     maxcolrights = zeros(gl.ncols)
@@ -274,6 +342,16 @@ function solve(ua::AxisLayout, innerbbox)
     SolvedAxisLayout(innerbbox, Main.BBox(ol, or, ot, ob), ua.axis)
 end
 
+"""
+This function allows indexing syntax to add a layout object to a grid.
+You can do:
+
+grid[1, 1] = obj
+grid[1, :] = obj
+grid[1:3, 2:5] = obj
+
+and all combinations of the above
+"""
 Base.setindex!(g, a::Alignable, rows::S, cols::T) where {T<:Union{UnitRange,Int,Colon}, S<:Union{UnitRange,Int,Colon}} = begin
 
     if typeof(rows) <: Int
@@ -306,23 +384,38 @@ function axislines!(scene, rect)
     lines!(scene, points, linewidth=2, show_axis=false)
 end
 
+"""
+A cheaper function that tries to come up with usable tick locations for a given value range
+"""
 function locateticks(xmin, xmax)
+    # whats the distance?
     d = xmax - xmin
+    # which order of magnitude is the distance in?
     ex = log10(d)
+    # round the exponent to the closest one
     exrounded = round(ex)
+    # this factor is used so we can find integer steps that then map back to
+    # nice numbers in the given value range
     factor = 1 / 10 ^ (exrounded - 1)
 
+    # minimum needs to be at least the lower value times the scaling factor
     xminf = ceil(xmin * factor)
+    # maximum needs to be at most the higher value times the scaling factor
     xmaxf = floor(xmax * factor)
 
+    # xminf and xmaxf are now both integers that are in an order of magnitude around ten steps apart
     df = xmaxf - xminf
 
+    # step sizes we like
     steps = [5, 4, 2, 1]
 
+    # from the highest to the lowest step size, choose the first that fits at least
+    # two times between the end values (gives three ticks including the end value)
     for s in steps
         n, remainder = divrem(df, s)
         if n >= 2
             rang = 1:n
+            # calculate the ticks by dividing with the factor from above
             ticks = [xminf; [xminf + x * s for x in rang]] ./ factor
             return ticks
         end
@@ -340,6 +433,7 @@ function LayoutedAxis(parent::Scene)
     ticksnode = Node(Point2f0[])
     ticks = linesegments!(parent, ticksnode, linewidth=2)[end]
 
+    # the algorithm from above seems to not give more than 7 ticks with the step sizes I chose
     nmaxticks = 7
 
     xticklabelnodes = [Node("0") for i in 1:nmaxticks]
@@ -379,6 +473,12 @@ function LayoutedAxis(parent::Scene)
 
         xtickvals = locateticks(xrange...)
         # xtickvals, vminbest, vmaxbest = optimize_ticks(xrange...)
+
+
+        # this code here tries to transform between values given in pixels of the
+        # scene and the camera area, but this is incorrect right now and everything
+        # should just be determined by the now unused limits that are saved in the
+        # LayoutedAxis object
         xfractions = (xtickvals .- xrange[1]) ./ width
         xrange_scene = (pxa.origin[1], pxa.origin[1] + pxa.widths[1])
         width_scene = xrange_scene[2] - xrange_scene[1]
@@ -404,6 +504,7 @@ function LayoutedAxis(parent::Scene)
         ytickends = [t + Point(-ticksize, 0.0) for t in ytickstarts]
 
 
+        # set and position tick labels
         xtickstrings = Showoff.showoff(xtickvals, :plain)
         nxticks = length(xtickvals)
         for i in 1:nmaxticks
@@ -428,7 +529,7 @@ function LayoutedAxis(parent::Scene)
             end
         end
 
-
+        # set tick mark positions
         ticksnode[] = collect(Iterators.flatten(zip(
            [xtickstarts; ytickstarts],
            [xtickends; ytickends])))
@@ -517,6 +618,8 @@ begin
     sg = outersolve(gl, BBox(shrinkbymargin(pixelarea(scene)[], 30)))
     applylayout(sg)
 
+    # when the scene is resized, apply the outersolve'd outermost grid layout
+    # this recursively updates all layout objects that are contained in the grid
     on(scene.events.window_area) do area
         sg = outersolve(gl, BBox(shrinkbymargin(pixelarea(scene)[], 30)))
         applylayout(sg)

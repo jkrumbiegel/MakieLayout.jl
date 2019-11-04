844e5247b32ec3775f97f0390f7f9117b60f7a48
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-11-04 10:17:12 +0100

    use inside outside solve method

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,23 +25,23 @@ scatter!(la4.scene, linkeddata, markersize=3, color=:orange, show_axis=false)
 scatter!(la5.scene, linkeddata, markersize=3, color=:pink, show_axis=false)
 update!(scene)
 
-gl = GridLayout([], 2, 2, [1, 1], [1, 1], 0.01, 0.01)
+gl = GridLayout([], 2, 2, [1, 1], [1, 1], 0.01, 0.01, Outside())
 
 gl[2, 1:2] = AxisLayout(BBox(75, 0, 0, 75), la1)
 gl[1, 2] = AxisLayout(BBox(75, 0, 0, 75), la2)
 
-gl2 = GridLayout([], 2, 2, [0.8, 0.2], [0.2, 0.8], 0.01, 0.01)
+gl2 = GridLayout([], 2, 2, [0.2, 0.8], [0.8, 0.2], 0.01, 0.01, Inside())
 gl2[2, 1] = AxisLayout(BBox(75, 0, 0, 75), la3)
 gl2[1, 1] = AxisLayout(BBox(75, 0, 0, 75), la4)
 gl2[2, 2] = AxisLayout(BBox(75, 0, 0, 75), la5)
 
 gl[1, 1] = gl2
 
-sg = outersolve(gl, BBox(shrinkbymargin(pixelarea(scene)[], 30)))
+sg = solve(gl, BBox(shrinkbymargin(pixelarea(scene)[], 30)))
 applylayout(sg)
 # when the scene is resized, apply the outersolve'd outermost grid layout
 # this recursively updates all layout objects that are contained in the grid
 on(scene.events.window_area) do area
-    sg = outersolve(gl, BBox(shrinkbymargin(pixelarea(scene)[], 30)))
+sg = solve(gl, BBox(shrinkbymargin(pixelarea(scene)[], 30)))
     applylayout(sg)
 end

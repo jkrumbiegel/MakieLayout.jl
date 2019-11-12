8c7e2bb7615f3c4bf46a2064f035cb7161bc093e
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-11-12 14:20:19 +0100

    add LayoutedText and remove BoxLayout aligns for it, plus example

---
+++ test/example4.jl
@@ -0,0 +1,30 @@
+using MakieLayout
+using Makie
+using FreeTypeAbstraction
+
+boldface = newface(expanduser("~/Library/Fonts/SFHelloSemibold.ttf"))
+
+begin
+    scene = Scene(resolution = (1000, 1000), font="SF Hello");
+    screen = display(scene)
+    campixel!(scene);
+
+    maingl = GridLayout(
+        1, 1;
+        parent = scene,
+        alignmode = Outside(30, 30, 30, 30)
+    )
+
+    las = Array{LayoutedAxis, 2}(undef, 4, 4)
+
+    for i in 1:4, j in 1:4
+        las[i, j] = maingl[i, j] = LayoutedAxis(scene)
+    end
+
+    lt = maingl[0, :] = LayoutedText(scene, text="Suptitle", textsize=50, font=boldface)
+    lt2 = maingl[2:5, 5] = LayoutedText(scene, text="Side Title", textsize=50,
+        rotation = -pi/2, font=boldface)
+    lt3 = maingl[6, 1:4] = LayoutedText(scene, text="Sub Title", textsize=50, font=boldface)
+    lt3 = maingl[2:5, 0] = LayoutedText(scene, text="Left Title", textsize=50,
+        rotation=pi/2, font=boldface)
+end

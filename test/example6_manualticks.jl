6008ea53296df1f1dfe7d46f9d5b1dba9c6fd364
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-11-14 10:37:57 +0100

    add manual ticks

---
+++ test/example6_manualticks.jl
@@ -0,0 +1,24 @@
+using MakieLayout
+using Makie
+
+begin
+    scene = Scene(resolution = (1000, 1000), font="SF Hello");
+    screen = display(scene)
+    campixel!(scene);
+
+    maingl = GridLayout(
+        1, 1;
+        parent = scene,
+        alignmode = Outside(30, 30, 30, 30)
+    )
+
+    la = maingl[1, 1] = LayoutedAxis(scene)
+
+    poly!(la, BBox(0, 1, 4, 0), color=:blue)
+    poly!(la, BBox(1, 2, 7, 0), color=:red)
+    poly!(la, BBox(2, 3, 1, 0), color=:green)
+
+    la.attributes.xticks = ManualTicks([0.5, 1.5, 2.5], ["blue", "red", "green"])
+    la.attributes.xlabel = "Color"
+    la.attributes.ylabel = "Value"
+end

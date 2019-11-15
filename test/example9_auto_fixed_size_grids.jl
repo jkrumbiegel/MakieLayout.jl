63f097d5d63c3fa7b5156e523e08d3f6de96dd6c
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-11-15 16:43:52 +0100

    add example for fixed grid

--- test/example9_auto_fixed_size_grids.jl
+++ test/example9_auto_fixed_size_grids.jl
@@ -8,6 +8,7 @@ begin
 
     maingl = GridLayout(
         1, 2;
+        addedcolgaps = Fixed(20),
         rowsizes = Relative(1),
         parent = scene,
         alignmode = Outside(30, 30, 30, 30)
@@ -17,15 +18,21 @@ begin
 
     guigl = maingl[1, 1] = GridLayout(1, 2)
 
-    guigl[1, 1] = LayoutedText(scene, text="Hello", halign=:left)
+    t1 = guigl[1, 1] = LayoutedText(scene, text="Hello", halign=:left)
+
     guigl[1, 2] = LayoutedButton(scene, 100, 30, "button")
 
-    guigl[end+1, 1] = LayoutedText(scene, text="World", halign=:left)
+    guigl[end+1, 1] = LayoutedText(scene, text="HelloHello", halign=:left)
     guigl[end, 2] = LayoutedButton(scene, 100, 30, "button")
+    #
+    # guigl[end+1, 1] = LayoutedText(scene, text="Whazzup", halign=:left)
+    # guigl[end, 2] = LayoutedButton(scene, 100, 30, "button")
 
-    guigl[end+1, 1] = LayoutedText(scene, text="Whazzup", halign=:left)
-    guigl[end, 2] = LayoutedButton(scene, 100, 30, "button")
-
-    guigl[end+1, :] = LayoutedSlider(scene, 30, 0:0.1:50)
+    # guigl[end+1, :] = LayoutedSlider(scene, 30, 0:0.1:50)
 
 end
+
+guigl.addedcolgaps = [Fixed(0)]
+t1.attributes.halign = :right
+t1.attributes.rotation = 0.0
+t1.attributes.padding = (0, 0, 0, 0)

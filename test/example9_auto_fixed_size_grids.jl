5cccb1f408c2dc82c18c3730ed2de0162efff13f
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-11-15 18:10:46 +0100

    change example 9

--- test/example9_auto_fixed_size_grids.jl
+++ test/example9_auto_fixed_size_grids.jl
@@ -7,28 +7,30 @@ begin
     campixel!(scene);
 
     maingl = GridLayout(
-        1, 2;
-        addedcolgaps = Fixed(20),
+        1, 3;
+        addedcolgaps = Fixed(0),
         rowsizes = Relative(1),
         parent = scene,
         alignmode = Outside(30, 30, 30, 30)
     )
 
     maingl[1, 2] = LayoutedAxis(scene)
+    maingl[1, 3] = LayoutedAxis(scene)
 
-    guigl = maingl[1, 1] = GridLayout(1, 2)
+    guigl = maingl[1, 1] = GridLayout(1, 3)
 
-    t1 = guigl[1, 1] = LayoutedText(scene, text="Hello", halign=:left)
+    # guigl[1, 1] = LayoutedButton(scene, 100, 30, "button")
+    guigl[1, 1] = LayoutedText(scene, text="HelloWorld", halign=:left)
+    guigl[1, 2] = LayoutedText(scene, text="Blablo", halign=:left)
+    guigl[1, 3] = LayoutedButton(scene, 120, 30, "Bliblu")
 
-    guigl[1, 2] = LayoutedButton(scene, 100, 30, "button")
+    guigl[2, 1] = LayoutedText(scene, text="Mamama", halign=:left)
+    guigl[2, 2] = LayoutedText(scene, text="Mimimi", halign=:left)
+    guigl[2, 3] = LayoutedButton(scene, 120, 30, "Momomo")
 
-    guigl[end+1, 1] = LayoutedText(scene, text="HelloHello", halign=:left)
-    guigl[end, 2] = LayoutedButton(scene, 100, 30, "button")
-    #
-    # guigl[end+1, 1] = LayoutedText(scene, text="Whazzup", halign=:left)
-    # guigl[end, 2] = LayoutedButton(scene, 100, 30, "button")
-
-    # guigl[end+1, :] = LayoutedSlider(scene, 30, 0:0.1:50)
+    guigl[3, 1] = LayoutedText(scene, text="VeerrryLoong", halign=:left)
+    guigl[3, 2] = LayoutedText(scene, text="Blablo", halign=:left)
+    guigl[3, 3] = LayoutedButton(scene, 120, 30, "Short")
 
 end
 

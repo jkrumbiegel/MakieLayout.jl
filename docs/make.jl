ce5e83a4479e81ec9e1559f971d072942262735e
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-11-17 16:37:16 +0100

    separate doc files

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,11 @@ using Documenter, MakieLayout, Makie
 
 makedocs(
     sitename="MakieLayout.jl",
+    pages = [
+        "index.md",
+        "Grids" => "grids.md",
+        "LayoutedAxis" => "layoutedaxis.md",
+    ],
     format = Documenter.HTML(
             prettyurls = get(ENV, "CI", nothing) == "true"
         )
---
+++ docs/src/layoutedaxis.md
@@ -0,0 +1,1 @@
+## Layouted Axis

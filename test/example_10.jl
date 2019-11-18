4e1ac4a763f7caa977f4ebcac3abf2ea4990483c
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-11-18 09:19:49 +0100

    add property convenience functions for layoutedaxis

--- test/example_10.jl
+++ test/example_10.jl
@@ -16,21 +16,21 @@ begin
 
     las = [maingl[i, j] = LayoutedAxis(scene) for i in 1:nrows, j in 1:ncols]
 
+    linkxaxes!(las...)
+    linkyaxes!(las...)
+
     for i in 1:nrows, j in 1:ncols
 
         scatter!(las[i, j], rand(200, 2) .+ [i j])
 
-        i > 1 && (las[i, j].attributes.titlevisible = false)
-        j > 1 && (las[i, j].attributes.ylabelvisible = false)
-        j > 1 && (las[i, j].attributes.yticklabelsvisible = false)
-        j > 1 && (las[i, j].attributes.yticksvisible = false)
-        i < nrows && (las[i, j].attributes.xticklabelsvisible = false)
-        i < nrows && (las[i, j].attributes.xticksvisible = false)
-        i < nrows && (las[i, j].attributes.xlabelvisible = false)
+        i > 1 && (las[i, j].titlevisible = false)
+        j > 1 && (las[i, j].ylabelvisible = false)
+        j > 1 && (las[i, j].yticklabelsvisible = false)
+        j > 1 && (las[i, j].yticksvisible = false)
+        i < nrows && (las[i, j].xticklabelsvisible = false)
+        i < nrows && (las[i, j].xticksvisible = false)
+        i < nrows && (las[i, j].xlabelvisible = false)
     end
-
-    linkxaxes!(las...)
-    linkyaxes!(las...)
 
     maingl[0, :] = LayoutedText(scene, text="Super Title", textsize=50)
     maingl[2:end, end+1] = LayoutedText(scene, text="Side Title", textsize=50, rotation=-pi/2)

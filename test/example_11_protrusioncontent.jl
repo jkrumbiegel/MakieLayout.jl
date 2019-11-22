e735b8bb63c5d1b768c4817d625c7497663d4b65
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-11-22 10:23:54 +0100

    add ProtrusionContentLayout plus example with cell numbers

---
+++ test/example_11_protrusioncontent.jl
@@ -0,0 +1,65 @@
+using MakieLayout
+using Makie
+
+begin
+    scene = Scene(resolution = (1000, 1000));
+    screen = display(scene)
+    campixel!(scene);
+
+    nrows = 4
+    ncols = 4
+
+    maingl = GridLayout(1, 1, parent=scene, alignmode=Outside(30))
+
+    gridgl = maingl[1, 1] = GridLayout(
+        nrows, ncols)
+
+    las = [gridgl[i, j] = LayoutedAxis(scene) for i in 1:nrows, j in 1:ncols]
+
+    linkxaxes!(las...)
+    linkyaxes!(las...)
+
+    for i in 1:nrows, j in 1:ncols
+
+        scatter!(las[i, j], rand(200, 2) .+ [i j], markersize=20, color=(:black, 0.3))
+
+        i > 1 && (las[i, j].titlevisible = false)
+        j > 1 && (las[i, j].ylabelvisible = false)
+        j > 1 && (las[i, j].yticklabelsvisible = false)
+        j > 1 && (las[i, j].yticksvisible = false)
+        i < nrows && (las[i, j].xticklabelsvisible = false)
+        i < nrows && (las[i, j].xticksvisible = false)
+        las[i, j].xlabelvisible = false
+    end
+
+    # gridgl[:, end, :right] = LayoutedText(scene, text="Side Protrusion", rotation=-pi/2, padding=(20, 0, 0, 0), halign=:right)
+    gridgl[end, :, :bottom] = LayoutedText(scene, text="x label", padding=(0, 0, 50, 0), valign=:bottom)
+
+    maingl[2, 1] = LayoutedAxis(scene)
+    maingl[:, 2] = LayoutedAxis(scene)
+
+    gridgl[:, :, :topleft] = LayoutedText(scene, text="A", textsize=50, padding=(0, 30, 0, 30))
+    maingl[2, 1, :topleft] = LayoutedText(scene, text="B", textsize=50, padding=(0, 30, 0, 30))
+    maingl[:, 2, :topleft] = LayoutedText(scene, text="C", textsize=50, padding=(0, 30, 0, 30))
+
+    # tl = maingl[0, :] = LayoutedText(scene, text="Super Title", textsize=50)
+    # stl = maingl[2:end, end+1] = LayoutedText(scene, text="Side Title", textsize=50, rotation=-pi/2)
+    #
+    # slgl = maingl[end+1, 1:end-1] = GridLayout(1, 2)
+    #
+    # slgl[1, 1] = LayoutedText(scene, text="Supertitle Size", halign=:left)
+    # sl1 = slgl[1, 2] = LayoutedSlider(scene, 30, 1:200)
+    # on(sl1.slider.value) do val
+    #     tl.attributes.textsize = val
+    # end
+    #
+    # slgl[2, 1] = LayoutedText(scene, text="Sidetitle Size", halign=:left)
+    # sl2 = slgl[2, 2] = LayoutedSlider(scene, 30, 1:200)
+    # on(sl2.slider.value) do val
+    #     stl.attributes.textsize = val
+    # end
+
+    nothing
+end
+
+save("layout.png", scene)

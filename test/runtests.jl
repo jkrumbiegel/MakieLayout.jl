e965dd4c6ded33b110e1a0fe6c3793ad3658ae95
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-11-23 16:01:08 +0100

    add test run over examples

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,16 @@
 using MakieLayout
 using Makie
+using Test
+
+@testset "examples" begin
+
+    println(readdir())
+    for file in readdir()
+        if startswith(file, "example") && endswith(file, ".jl")
+            println("Executing $file")
+            include(file)
+            sleep(2) # for seeing the result briefly
+        end
+    end
+
+end

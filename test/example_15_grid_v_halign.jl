1f3d29bd1a9893b50e541b6b58442e0b61af15ea
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-11-27 15:08:47 +0100

    updated example with align demo buttons

--- test/example_15_grid_v_halign.jl
+++ test/example_15_grid_v_halign.jl
@@ -10,7 +10,7 @@ begin
 
     innergrid = outergrid[1, 1] = GridLayout(3, 3)
 
-    las = [innergrid[i, 1] = LayoutedAxis(scene) for i in 1:3]
+    las = innergrid[:, 1] = [LayoutedAxis(scene) for i in 1:3]
 
     alignedgrid1 = innergrid[:, 2] = GridLayout(2, 1; rowsizes=Relative(0.33), valign=:center)
     alignedgrid1[1, 1] = LayoutedAxis(scene)
@@ -18,4 +18,17 @@ begin
 
     alignedgrid2 = innergrid[:, 3] = GridLayout(rowsizes=Relative(0.5); valign=:bottom)
     alignedgrid2[1, 1] = LayoutedAxis(scene)
+
+    buttonsgl = innergrid[end+1, :] = GridLayout()
+
+    valigns = (:bottom, :top, :center)
+
+    buttons = buttonsgl[1, 1:3] = [LayoutedButton(scene; height=40, label="$v") for v in valigns]
+
+    for (button, align) in zip(buttons, valigns)
+        on(button.button.clicks) do c
+            alignedgrid1.valign[] = align
+            alignedgrid2.valign[] = align
+        end
+    end
 end

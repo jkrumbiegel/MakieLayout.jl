63864f04bba062df71584d77cd78d6962d16bf98
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-12-01 14:59:19 +0100

    add vertical sliders

--- test/example_18_new_sliders.jl
+++ test/example_18_new_sliders.jl
@@ -15,14 +15,16 @@ begin
     slidergrid = innergrid[2, 1] = GridLayout()
 
     ls1 = slidergrid[1, 1] = LayoutedSlider(scene, range=50:0.01:100, height=50)
-    slidergrid[1, 2] = LayoutedText(scene, text=lift(format, ls1.value),
-        alignment=(:left, :center), width=140, padding=(5, 5, 5, 5))
+    slidergrid[1, 2] = LayoutedText(scene, text=lift(x->@sprintf("%.2f", x), ls1.value),
+        alignment=(:right, :center), width=140, padding=(5, 5, 5, 5))
     slidergrid[1, 2] = LayoutedRect(scene, color=:white)
 
     ls2 = slidergrid[2, 1] = LayoutedSlider(scene, range=LinRange(0.1, 3, 1000), startvalue=1, height=50)
-    slidergrid[2, 2] = LayoutedText(scene, text=lift(format, ls2.value),
-        alignment=(:left, :center), width=140, padding=(5, 5, 5, 5))
+    slidergrid[2, 2] = LayoutedText(scene, text=lift(x->@sprintf("%.2f", x), ls2.value),
+        alignment=(:right, :center), width=140, padding=(5, 5, 5, 5))
     slidergrid[2, 2] = LayoutedRect(scene, color=:white)
 
     lines!(la, lift(x -> sin.((0:0.05:10) .* x), ls2.value))
+
+    innergrid[:, end+1] = LayoutedSlider(scene, range=0:100, width=50, horizontal=false)
 end

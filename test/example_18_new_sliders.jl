325dff6af7d5a09c1fa69641d890adafc8038c86
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-12-01 13:10:39 +0100

    update example

--- test/example_18_new_sliders.jl
+++ test/example_18_new_sliders.jl
@@ -12,7 +12,9 @@ begin
 
     innergrid[1, :] = LayoutedAxis(scene)
     ls1 = innergrid[2, :] = LayoutedSlider(scene, range=50:0.01:100, height=50)
-    innergrid[3, :] = LayoutedSlider(scene, height=50)
+    ls2 = innergrid[3, :] = LayoutedSlider(scene, range=50:0.01:100, height=50)
 
     innergrid[0, :] = LayoutedText(scene, textsize=ls1.value)
+    innergrid[2, end+1] = LayoutedText(scene, textsize=ls2.value)
+    rowsize!(innergrid, 2, Auto(false, 1))
 end

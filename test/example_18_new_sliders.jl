a8f3ad15620f466a52aba5899b30a0c37a05819d
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-12-02 12:39:02 +0100

    update slider example

--- test/example_18_new_sliders.jl
+++ test/example_18_new_sliders.jl
@@ -2,6 +2,7 @@ using Makie
 using MakieLayout
 using Printf
 
+
 begin
     scene = Scene(camera=campixel!)
     screen = display(scene)
@@ -10,22 +11,44 @@ begin
 
     innergrid = outergrid[1, 1] = GridLayout()
 
-    la = innergrid[1, 1] = LayoutedAxis(scene)
+    lagrid = innergrid[1, 1] = GridLayout()
+    la = lagrid[1, 1] = LayoutedAxis(scene)
+    # la.xoppositespinevisible = false
+    # la.yoppositespinevisible = false
+    la.xgridvisible = false
+    la.ygridvisible = false
+    la.xautolimitmargin = (0, 0)
+    la.yautolimitmargin = (0, 0)
+
+
 
     slidergrid = innergrid[2, 1] = GridLayout()
 
     ls1 = slidergrid[1, 1] = LayoutedSlider(scene, range=50:0.01:100, height=50)
     slidergrid[1, 2] = LayoutedText(scene, text=lift(x->@sprintf("%.2f", x), ls1.value),
         alignment=(:right, :center), width=140, padding=(5, 5, 5, 5))
-    slidergrid[1, 2] = LayoutedRect(scene, color=:white, strokewidth=1f0)
+    slidergrid[1, 2] = LayoutedRect(scene, color=:white, strokewidth=2f0, strokecolor=RGBf0(0.9, 0.9, 0.9))
 
     ls2 = slidergrid[2, 1] = LayoutedSlider(scene, range=LinRange(0.1, 3, 1000), startvalue=1, height=50)
     slidergrid[2, 2] = LayoutedText(scene, text=lift(x->@sprintf("%.2f", x), ls2.value),
         alignment=(:right, :center), width=140, padding=(5, 5, 5, 5))
-    slidergrid[2, 2] = LayoutedRect(scene, color=:white, strokewidth=1f0)
+    slidergrid[2, 2] = LayoutedRect(scene, color=:white, strokewidth=2f0, strokecolor=RGBf0(0.9, 0.9, 0.9))
 
     ls3 = innergrid[1, end+1] = LayoutedSlider(scene, range=0:0.01:1, width=50, horizontal=false)
 
-    lines!(la, lift((x, y)-> sin.((0:0.05:10) .* x) .* y, ls2.value, ls3.value))
+    buttongrid = slidergrid[3, :] = GridLayout()
+    buttongrid[1, 1] = LayoutedButton(scene, height=40)
+    b2 = buttongrid[1, 2] = LayoutedButton(scene, autoshrink=(false, false), label="Change colormap")
 
+    data = lift(ls2.value, ls3.value) do v1, v2
+        [sin(x * y / 1000 * v1) * v2 for x in 1:100, y in 1:100]
+    end
+
+    hmap = heatmap!(la, data)
+
+    lagrid[1, 2] = LayoutedColorbar(scene, hmap, width=40)
+
+    on(b2.clicks) do c
+        hmap.colormap = rand(setdiff([:viridis, :heat, :rainbow, :blues], [hmap.colormap[]]))
+    end
 end

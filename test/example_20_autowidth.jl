3730d916437714b12a3b76a92ccf9a08fbc64a07
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-12-04 11:26:16 +0100

    example with auto size for buttons

---
+++ test/example_20_autowidth.jl
@@ -0,0 +1,32 @@
+using MakieLayout
+using Makie
+
+
+begin
+    scene = Scene(camera=campixel!)
+    display(scene)
+
+    topgl = GridLayout(parent=scene, alignmode=Outside(30))
+    gl = topgl[1, 1] = GridLayout()
+    la = gl[1, 1] = LayoutedAxis(scene)
+    lc2 = gl[1, 2] = LayoutedColorbar(scene, width=50)
+
+    gl[2, :] = LayoutedSlider(scene, height=40, range=1:1000)
+    bgl = gl[3, :] = GridLayout()
+    bs = bgl[1, 1:5] = [LayoutedButton(scene, height=40, width=Auto(), label="xx" ^ i) for i in 1:5]
+    bgl2 = gl[4, :] = GridLayout()
+    b1 = bgl2[1, 1] = LayoutedButton(scene, width = Auto(), label="prev")
+    sl1 = bgl2[1, 2] = LayoutedSlider(scene, height=40)
+    b2 = bgl2[1, 3] = LayoutedButton(scene, width = Auto(), label="next")
+
+    te = gl[0, :] = LayoutedText(scene, text="Buttons on Buttons", textsize=50)
+
+    nothing
+end
+
+bs[1].attributes.padding = (0, 0, 0, 0)
+bs[1].attributes.height = Auto()
+
+la.aspect = nothing
+
+te.rotation

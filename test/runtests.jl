091598d2ddcd23bb0a139743f6048cfbfbf62242
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-01-25 11:09:44 +0100

    add Mixed AlignMode plus tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,11 @@ using Test
 
 include("debugrect.jl")
 
+# have BBoxes show as such because the default is very verbose
+Base.show(io::IO, bb::BBox) = print(io, "BBox(l: $(left(bb)), r: $(right(bb)), b: $(bottom(bb)), t: $(top(bb)))")
 
-@testset "GridLayout 1" begin
+
+@testset "GridLayout Zero Outside AlignMode" begin
     bbox = BBox(0, 1000, 0, 1000)
     layout = GridLayout(bbox = bbox, alignmode = Outside(0))
     dr = layout[1, 1] = DebugRect()
@@ -26,4 +29,67 @@ include("debugrect.jl")
     colgap!(layout, 1, Fixed(0))
 
     @test dr.layoutnodes.computedbbox[].widths == dr2.layoutnodes.computedbbox[].widths == Float32[400.0, 800.0]
+end
+
+@testset "GridLayout Outside AlignMode" begin
+    bbox = BBox(0, 1000, 0, 1000)
+    layout = GridLayout(bbox = bbox, alignmode = Outside(100, 200, 50, 150))
+    dr = layout[1, 1] = DebugRect()
+
+    @test dr.layoutnodes.computedbbox[] == BBox(100, 800, 50, 850)
+
+    dr.topprot = 100
+    @test dr.layoutnodes.computedbbox[] == BBox(100, 800, 50, 750)
+    dr.bottomprot = 100
+    @test dr.layoutnodes.computedbbox[] == BBox(100, 800, 150, 750)
+    dr.leftprot = 100
+    @test dr.layoutnodes.computedbbox[] == BBox(200, 800, 150, 750)
+    dr.rightprot = 100
+    @test dr.layoutnodes.computedbbox[] == BBox(200, 700, 150, 750)
+end
+
+@testset "GridLayout Inside AlignMode" begin
+    bbox = BBox(0, 1000, 0, 1000)
+    layout = GridLayout(bbox = bbox, alignmode = Inside())
+    dr = layout[1, 1] = DebugRect()
+
+    @test dr.layoutnodes.computedbbox[] == BBox(0, 1000, 0, 1000)
+
+    dr.topprot = 100
+    @test dr.layoutnodes.computedbbox[] == BBox(0, 1000, 0, 1000)
+    dr.bottomprot = 100
+    @test dr.layoutnodes.computedbbox[] == BBox(0, 1000, 0, 1000)
+    dr.leftprot = 100
+    @test dr.layoutnodes.computedbbox[] == BBox(0, 1000, 0, 1000)
+    dr.rightprot = 100
+    @test dr.layoutnodes.computedbbox[] == BBox(0, 1000, 0, 1000)
+end
+
+@testset "GridLayout Mixed AlignMode" begin
+    bbox = BBox(0, 1000, 0, 1000)
+    layout = GridLayout(bbox = bbox, alignmode = Mixed(left = 0, top = 100))
+    dr = layout[1, 1] = DebugRect()
+
+    @test MakieLayout.protrusion(layout, Left()) == 0
+    @test MakieLayout.protrusion(layout, Right()) == 0
+    @test MakieLayout.protrusion(layout, Bottom()) == 0
+    @test MakieLayout.protrusion(layout, Top()) == 0
+
+    @test dr.layoutnodes.computedbbox[] == BBox(0, 1000, 0, 900)
+
+    dr.topprot = 100
+    @test MakieLayout.protrusion(layout, Top()) == 0
+    @test dr.layoutnodes.computedbbox[] == BBox(0, 1000, 0, 800)
+
+    dr.bottomprot = 100
+    @test MakieLayout.protrusion(layout, Bottom()) == 100
+    @test dr.layoutnodes.computedbbox[] == BBox(0, 1000, 0, 800)
+
+    dr.leftprot = 100
+    @test MakieLayout.protrusion(layout, Left()) == 0
+    @test dr.layoutnodes.computedbbox[] == BBox(100, 1000, 0, 800)
+
+    dr.rightprot = 100
+    @test MakieLayout.protrusion(layout, Right()) == 100
+    @test dr.layoutnodes.computedbbox[] == BBox(100, 1000, 0, 800)
 end

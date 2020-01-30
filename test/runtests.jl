6973d82a675ce6a247f8e94352e3e421e6095b32
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-01-30 12:52:28 +0100

    test layoutables creation (doesn't need GPU without display)

--- test/runtests.jl
+++ test/runtests.jl
@@ -93,3 +93,17 @@ end
     @test MakieLayout.protrusion(layout, Right()) == 100
     @test dr.layoutnodes.computedbbox[] == BBox(100, 1000, 0, 800)
 end
+
+@testset "Layoutables constructors" begin
+    scene, layout = layoutscene()
+    ax = layout[1, 1] = LAxis(scene)
+    cb = layout[1, 2] = LColorbar(scene)
+    gl2 = layout[2, :] = GridLayout()
+    bu = gl2[1, 1] = LButton(scene)
+    sl = gl2[1, 2] = LSlider(scene)
+
+    scat = scatter!(ax, rand(10))
+    le = gl2[1, 3] = LLegend(scene, [scat], ["scatter"])
+
+    to = gl2[1, 4] = LToggle(scene)
+end
